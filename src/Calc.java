d30695291eeca8654185e755a0e3cde4de415d10
Nev1ps <127889055+Nev1ps@users.noreply.github.com>
2024-02-20 13:34:05 +0300

    first commit

---
+++ src/Calc.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите пример: ");
+        String input = scanner.nextLine();
+        try {
+            String[] parts = input.split(" ");
+            if (parts.length !=3) {
+                throw new IllegalArgumentException("Неправильный формат ввода");
+            }
+            int a = Integer.parseInt(parts[0]);
+            char op = parts[1].charAt(0);
+            int b = Integer.parseInt(parts[2]);
+
+            if (a > 10 || b > 10) {
+                throw new IllegalArgumentException("Вводимое число больше 10");
+            }
+            int result;
+            switch (op) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    if (b != 0) result = a / b;
+                    else throw new ArithmeticException("Деление на ноль!");
+                    break;
+                default:
+                    throw new IllegalArgumentException("Неверный оператор!");
+            }
+
+            System.out.println("Результат: " + result);
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        scanner.close();
+    }
+}
